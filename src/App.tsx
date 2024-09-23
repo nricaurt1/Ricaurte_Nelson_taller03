264374538b2539ac30a9a85b7408fd542ba8419f
nricaurt1 <nricaurt1@cuc.edu.co>
2024-09-23 09:02:04 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ae from './assets/ae.jpg'
+
+function App() {
+	return (
+		<>
+			<img src={ae} alt="ae" width="300" height="100"/>
+			<h1>ae</h1>
+			<p>ae</p>
+		</>
+	)
+}
+
+export default App
